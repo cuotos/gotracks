1dff4efedaf057b6ac7b8f8f0129afe527e9eb89
Dan Potepa <dan@bloomandwild.com>
2021-12-22 17:24:31 +0000

    grinding

--- cmd/tab.go
+++ cmd/tab.go
@@ -9,26 +9,53 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	UGSearchURL = "https://www.ultimate-guitar.com/search.php"
+)
+
 var tabCmd = &cobra.Command{
 	Use: "tab",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return openUG()
+		ugc := UGClient{
+			baseSearchURL: UGSearchURL,
+		}
+
+		track, err := track.GetCurrentTrack()
+		if err != nil {
+			return err
+		}
+
+		url, err := ugc.generateSearchURL(track)
+		if err != nil {
+			return err
+		}
+
+		_, err = mack.Tell("Google Chrome", fmt.Sprintf(`open location "%v"`, url.String()))
+
+		if err != nil {
+			return fmt.Errorf("unable to open browser: %w", err)
+		}
+		return nil
 	},
 }
 
-func openUG() error {
-	track, err := track.GetCurrentTrack()
-	if err != nil {
-		return err
-	}
+type UGClient struct {
+	baseSearchURL string
+}
+
+func (ugClient *UGClient) generateSearchURL(track track.Track) (*url.URL, error) {
 
 	searchText := url.QueryEscape(fmt.Sprintf("%v %v", track.Artist, track.Title))
-	searchUri := fmt.Sprintf("https://www.ultimate-guitar.com/search.php?search_type=title&value=%v", searchText)
 
-	_, err = mack.Tell("Google Chrome", fmt.Sprintf(`open location "%v"`, searchUri))
+	ugUrl, err := url.Parse(ugClient.baseSearchURL)
 	if err != nil {
-		return fmt.Errorf("unable to open browser: %w", err)
+		return nil, fmt.Errorf("failed to parse ultimate guitar search url: %w", err)
 	}
 
-	return nil
+	q := ugUrl.Query()
+	q.Add("search_type", "title")
+	q.Add("value", searchText)
+	ugUrl.RawQuery = q.Encode()
+
+	return ugUrl, nil
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/andybrewer/mack v0.0.0-20200226161639-15be3d47cc54
-	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/cobra v1.2.1
+	github.com/stretchr/testify v1.7.0
 )
